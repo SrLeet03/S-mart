5b412eb5f774b1d8c47edb62706d02cd18ce9cce
ant-hunt123 <sarveshraut123@gmail.com>
2020-07-29 06:58:14 +0530

    User authentication with webtokens

--- projectbackend/controllers/auth.js
+++ projectbackend/controllers/auth.js
@@ -1,6 +1,16 @@
 const User = require('../models/user')
+const {check , validationResult} = require('express-validator');
+const jwt = require('jsonwebtoken')
+const expressJwt = require('express-jwt');
 
 exports.singup = (req , res ) =>{
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+           error : errors.array()[0].msg
+        });
+    }
    const user  = new User (req.body) ;
    user.save((err , user/* this can be anything*/) =>{
        if(err){
@@ -16,6 +26,35 @@ exports.singup = (req , res ) =>{
           }
        );
    });
+};
+exports.singin = (req , res) =>{
+    const {email,password} = req.body;
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({
+           error : errors.array()[0].msg
+        });
+    }
+    User.findOne({email} , (err,user)=>{
+        if(err){
+            res.status(400).json({
+                error:"User email does not exit"
+            })
+        }
+        if(user.autheticate(password)){
+          return res.status(401).json({
+              error:"Email and password does not match"
+          })
+        }
+        // CREATING OF token
+        const token = jwt.sing({_id :user._id} , process.env.SECRET)
+        //putting the toekn in web cokkie
+        res.cookie("token" , {expire:new Date()+99});
+
+        //send response to front end
+        const {_id  , name , email , role} = user ;
+        return res.json({token , user:{_id , name , email , role}})
+    })
 };
 exports.singout = (req , res ) =>{
     res.json({
--- projectbackend/routes/auth.js
+++ projectbackend/routes/auth.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const Router = express.Router();
-const {singup , singout} = require("../controllers/auth") ;
+const {singup , singout ,singin} = require("../controllers/auth") ;
+const {check , validationResult} = require('express-validator');
+Router.post("/singup" ,[
+check("name").isLength({min:3})
+.withMessage('name lemth gtrther than 3'),
+check("email").isEmail().withMessage('enter valid email'),
+check("password").isLength({min:5}).withMessage('min passward length is 5')
+], singup) ;
 
-Router.post("/singup" , singup) 
-Router.get("/singout" , singout) 
+Router.post("/singin" ,[
+    check("email").isEmail().withMessage('email is required'),
+    check("password").isLength({min:1}).withMessage('min passward length is 5')
+    ], singin) ;
 
-module.exports = Router ;
+Router.get("/singout" , singout)
+
+module.exports = Router  ;
