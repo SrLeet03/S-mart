481e6ab7297f3c07c0eaf7b157a51acea243b990
ant-hunt123 <sarveshraut123@gmail.com>
2020-08-01 21:19:39 +0530

    info

--- projectbackend/controllers/auth.js
+++ projectbackend/controllers/auth.js
@@ -92,7 +92,7 @@ exports.isAuthenticated = (req, res, next) => {
 };
 
 exports.isAdmin = (req, res, next) => {
-  if (req.profile.role === 0) {
+  if (req.profile.role === 0) { // bydefault 1
     return res.status(403).json({
       error: "You are not ADMIN, Access denied"
     });
